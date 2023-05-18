50c1b31de8394dde07923c7a4fa90209c2f36857
Ajoy31097 <balmikiajoy03@gmail.com>
2023-05-18 14:32:52 +0530

    Commit 2

---
+++ Gitstart/dom.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title')
+var header = document.getElementById('main-header');
+header.style.borderBottom = '3px solid #000';
+
+var addItem = document.getElementsByClassName('title')[0];
+addItem.style.fontWeight = 'bold';
+addItem.style.color = 'green';
